9133e064848b96a88de49fb1a9f425f48e10c741
vidhirathod29 <vidhirathod.shivinfotech@gmail.com>
2024-09-27 17:27:48 +0530

    changes: added condition to not deploy admin panel

--- my-project/config/server.ts
+++ my-project/config/server.ts
@@ -1,7 +1,10 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 6000),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']), 
+  },
+   admin: {
+    enabled: false,  
   },
 });
